9103252337ae1f119f5b48251b93b9bb4bd325a0
Phạm Minh Quang <quangpham.245@protonmail.com>
2022-11-04 00:29:28 +0700

    Implement env module

--- apps/api/libs/env/src/env.module.ts
+++ apps/api/libs/env/src/env.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { EnvService } from './env.service';
+import { envValidation } from './env-validation';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: envValidation,
+    }),
+  ],
   providers: [EnvService],
   exports: [EnvService],
 })
--- apps/api/libs/env/src/env.service.ts
+++ apps/api/libs/env/src/env.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class EnvService {}
+export class EnvService {
+  constructor(configService: ConfigService) {
+    this.env = configService.get('ENV');
+    this.baseUrl = configService.get('API_URL');
+    this.oidc;
+    this.oidc = {
+      google: {
+        clientId: configService.get('OIDC_GOOGLE_CLIENT_ID'),
+        clientSecret: configService.get('OIDC_GOOGLE_CLIENT_SECRET'),
+      },
+    };
+  }
+
+  public readonly env: string;
+
+  public readonly baseUrl: string;
+
+  public readonly oidc: {
+    google: OpenIdProviderSetting;
+  };
+}
+
+type OpenIdProviderSetting = {
+  clientId: string;
+  clientSecret: string;
+};
