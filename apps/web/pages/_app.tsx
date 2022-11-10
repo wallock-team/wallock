96065464b9269c06245e06b73c58272a41d8eb01
Phạm Minh Quang <quangpham.245@protonmail.com>
2022-11-10 22:05:37 +0700

    (web) add fonts

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,12 +1,17 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Wallock</title>
-        <link rel="icon" href="/public/favicon/favicon.ico" />
+        <link rel="icon" href="public/favicon/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
