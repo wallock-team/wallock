6ea9ec51e9daa12621f6f6301b87c62075eed511
Phạm Minh Quang <quangpham.245@protonmail.com>
2022-11-10 22:19:12 +0700

    (web) config next.config.js to get .env variables

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    API_URL: process.env.API_URL,
+    WEB_URL: process.env.WEB_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
