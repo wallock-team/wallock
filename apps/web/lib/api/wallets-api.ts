763dc267a41dd660e97d8f19ca325926ffa490ef
Phạm Minh Quang <quangpham.245@protonmail.com>
2022-11-12 02:19:46 +0700

    (web) add create wallet api

--- apps/web/lib/api/wallets-api.ts
+++ apps/web/lib/api/wallets-api.ts
@@ -1,8 +1,12 @@
-import { Wallet } from '@wallock/schemas';
+import { Wallet, WalletCreateDto } from '@wallock/schemas';
 import { Axios } from 'axios';
 
 export class WalletsApi {
   constructor(private readonly axios: Axios) {}
+
+  async createWallet(dto: WalletCreateDto) {
+    return (await this.axios.post<Wallet>('/wallets', dto)).data;
+  }
 
   async getWallets() {
     return (await this.axios.get<Wallet[]>('/wallets')).data;
