525fb0026e30ffc19615e9c3a9bb3474db536297
Phạm Minh Quang <quangpham.245@protonmail.com>
2022-11-12 01:46:26 +0700

    (schemas) add WalletCreateYup

--- packages/schemas/src/wallets/wallet-create.dto.ts
+++ packages/schemas/src/wallets/wallet-create.dto.ts
@@ -1,6 +1,10 @@
-import { IsNotEmpty } from 'class-validator';
-
+import { IsNotEmpty } from "class-validator";
+import * as yup from "yup";
 export class WalletCreateDto {
   @IsNotEmpty()
   name: string;
 }
+
+export const WalletCreateYup = yup.object({
+  name: yup.string().trim().required("Name is required"),
+});
