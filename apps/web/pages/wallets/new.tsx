fe68ec86d9746a7b6bbc7edc9b07d4e18da65d00
Phạm Minh Quang <quangpham.245@protonmail.com>
2022-11-11 15:33:28 +0700

    (web) add form validation to new wallet page

--- apps/web/pages/wallets/new.tsx
+++ apps/web/pages/wallets/new.tsx
@@ -1,9 +1,12 @@
-import { Container, Stack, TextField } from '@mui/material';
+import { Button, Container, Stack, TextField } from '@mui/material';
 import { Wallet } from '@wallock/schemas';
+import { WalletCreateDto } from '@wallock/schemas';
 import { GetServerSideProps, NextPage } from 'next';
 import { CancelOrConfirmAppBar } from '../../components/common/cancel-or-confirm-app-bar';
 import Api from '../../lib/api/api';
 import { withAuthPage } from '../../lib/with-auth-page';
+import { FormikErrors, useFormik } from 'formik';
+import { validate } from 'class-validator';
 
 type Props = {
   wallets: Wallet[];
@@ -13,21 +16,47 @@ export const getServerSideProps: GetServerSideProps<Props> = withAuthPage(
   async function (ctx) {
     const api = Api.fromWebServer(ctx);
 
+    const wallets = await api.wallets.getWallets();
+
     return {
       props: {
-        wallets: await api.wallets.getWallets(),
+        wallets,
       },
     };
   }
 );
 
 const NewWallet: NextPage<Props> = function (props: Props) {
+  const formik = useFormik<WalletCreateDto>({
+    initialValues: new WalletCreateDto(),
+    validate: async function (values) {
+      const errors = await validate(values);
+
+      const formikErrors: any = {};
+      errors.forEach((error) => {
+        if (!error.constraints) {
+          return;
+        }
+        formikErrors[error.property] = Object.values(error.constraints);
+      });
+
+      return formikErrors;
+    },
+    onSubmit: console.log,
+  });
+
   return (
     <>
       <CancelOrConfirmAppBar title="New Wallet" />
       <Container maxWidth="md" sx={{ mt: 4 }}>
         <Stack>
-          <TextField label="Name" />
+          <TextField
+            label="Name"
+            {...formik.getFieldProps('name')}
+            error={!!(formik.touched.name && formik.errors.name)}
+            helperText={formik.touched.name && formik.errors.name}
+          />
+          <Button onClick={formik.submitForm}>Submit</Button>
         </Stack>
       </Container>
     </>
