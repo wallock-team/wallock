3e861822588d559e5dcb8c8a6baa6819e7c0b282
Phạm Minh Quang <quangpham.245@protonmail.com>
2022-11-11 15:32:22 +0700

    (schemas) add file extensions

--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -1,12 +1,12 @@
 export { Base } from './common/base.entity.js';
 
-export { Meta } from './common/meta.entity';
+export { Meta } from './common/meta.entity.js';
 
 export { User } from './users/user.entity.js';
 export { UserCreateDto } from './users/user-create.dto.js';
 
-export { OpenId } from './users/open-id.entity';
+export { OpenId } from './users/open-id.entity.js';
 export { OpenIdCreateDto } from './users/open-id-create.dto.js';
 
-export { Wallet } from './wallets/wallet.entity';
-export { WalletCreateDto } from './wallets/wallet-create.dto';
+export { Wallet } from './wallets/wallet.entity.js';
+export { WalletCreateDto } from './wallets/wallet-create.dto.js';
